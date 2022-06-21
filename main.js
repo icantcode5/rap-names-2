41f83c2337bfb385243af66180ab9b46e2213578
Carlos Sosa <carlossosa54321@gmail.com>
2022-06-21 16:50:20 -0700

    simple api backend

---
+++ main.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('button')
+button.addEventListener.apply('click', _=> {
+
+})
+
+console.log('JS is connected to HTML')
---
+++ server.js
@@ -0,0 +1,40 @@
+//const { response } = require("express")
+
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage': {
+        'age': 29,
+        'birthName':"Sheyaa Bin Abraham",
+        'birthLocation' : 'London, England'
+        },
+    'chance the rapper': {
+        'age' : 29,
+        'birthName' : "Chancelor Bennet",
+        'birthLocation': 'Chicago, Illinois'},
+    'dylan': {
+        'age' : 'Dylan',
+        'birthName' : "Dylan",
+        'birthLocation': 'Dylan'
+        }
+}
+
+
+app.get('/', (request,response)=> {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rapperName', (request,response) => {
+   const rappersName =  request.params.rapperName.toLowerCase()
+    if(rappers[rappersName]){
+        response.json(rappers[rappersName])
+    }else{
+        response.json(rappers["dylan"])
+    }
+})
+
+app.listen(PORT, () => {
+    console.log('The server is running! Go catch it! LOL')
+})
